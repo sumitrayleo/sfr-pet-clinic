0adc7f5b5d1954397c378c6d8e1a11f709bfc8a1
Sumit Ray <sumitrayleo@gmail.com>
2020-04-01 13:23:42 +0530

    Initial checkin - setup

---
+++ src/main/java/ray/springframework/test/sfrpetclinic/SfrPetClinicApplication.java
@@ -0,0 +1,13 @@
+package ray.springframework.test.sfrpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfrPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfrPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ray/springframework/test/sfrpetclinic/SfrPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ray.springframework.test.sfrpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfrPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
