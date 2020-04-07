29938aac30c58efccf798a01ea111b2f25021b1a
Sumit Ray <sumitrayleo@gmail.com>
2020-04-07 22:30:35 +0530

    Added interface for pet clinic. closes #6

---
+++ pet-clinic-data/src/main/java/ray/springframework/sfrpetclinic/service/OwnerService.java
@@ -0,0 +1,12 @@
+package ray.springframework.sfrpetclinic.service;
+
+import ray.springframework.sfrpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/ray/springframework/sfrpetclinic/service/PetService.java
@@ -0,0 +1,11 @@
+package ray.springframework.sfrpetclinic.service;
+
+import ray.springframework.sfrpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
