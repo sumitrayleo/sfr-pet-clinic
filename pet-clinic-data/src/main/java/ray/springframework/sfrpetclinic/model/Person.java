45d729780ff3a2ff3e386eeb33cad97bde660fc7
Sumit Ray <sumitrayleo@gmail.com>
2020-04-07 22:38:04 +0530

    Added BaseEntity

--- pet-clinic-data/src/main/java/ray/springframework/sfrpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/ray/springframework/sfrpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package ray.springframework.sfrpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/ray/springframework/sfrpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/ray/springframework/sfrpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package ray.springframework.sfrpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
     private PetType petType;
     private Owner owner;
     private LocalDate dateOfBirth;
