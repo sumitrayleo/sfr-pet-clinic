d4b4872f9107ad5185bfd1fa031621c10221bce6
Sumit Ray <sumitrayleo@gmail.com>
2020-04-08 12:50:28 +0530

    Vets index page and controller. closes #15

---
+++ pet-clinic-web/src/main/java/ray/springframework/sfrpetclinic/controller/VetController.java
@@ -0,0 +1,13 @@
+package ray.springframework.sfrpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets/index.htm"})
+    public String listVets(){
+        return "vets/index";
+    }
+}
