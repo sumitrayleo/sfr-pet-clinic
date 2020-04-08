11252b3cad204a554b85290389129b90562cedc7
Sumit Ray <sumitrayleo@gmail.com>
2020-04-08 12:21:05 +0530

    Implemented index page and controller. closes #3

---
+++ pet-clinic-web/src/main/java/ray/springframework/sfrpetclinic/controller/IndexController.java
@@ -0,0 +1,13 @@
+package ray.springframework.sfrpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.htm", "index.html"})
+    public String index(){
+        return "index";
+    }
+}
