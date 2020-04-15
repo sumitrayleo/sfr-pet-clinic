d9bbd0b7779ac8ddb0c0175d39bd42a542c37946
Sumit Ray <sumitrayleo@gmail.com>
2020-04-15 21:25:25 +0530

    Added maven-surefire-plugin and Junit5. Closes #1

---
+++ pet-clinic-data/src/test/java/ray/springframework/sfrpetclinic/repository/OwnerRepositoryIT.java
@@ -0,0 +1,15 @@
+package ray.springframework.sfrpetclinic.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OwnerRepositoryIT {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByLastName() {
+    }
+}
